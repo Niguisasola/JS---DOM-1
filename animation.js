31c33cb1f32b79217b5848a944851620100ad74f
Nika <nikaguisasola@gmail.com>
2022-05-30 14:40:48 +0200

    1440h

--- animation.js
+++ animation.js
@@ -1,5 +1,9 @@
 let listaCompras = ['manzana', 'tomate', 'agua', 'espinaca', 'quinoa']
     console.dir(listaCompras)
     
-
 document.write("<h1>My title</h1>")
+
+function changeButton() {
+    document.getElementById("btn").style.background='blue'
+    document.getElementById("btn").style.borderRadius='50%'
+}
